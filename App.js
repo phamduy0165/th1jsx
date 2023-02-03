8aa675a23bad10541df7f67610bd8de424be3593
phamduy0165 <75572336+phamduy0165@users.noreply.github.com>
2023-02-03 18:28:18 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h3>Hello Global</h3>
+    </div>
+  );
+}
+
+export default App;
